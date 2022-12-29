29df59ae2833203cac50ed751638c20404fffadd
João <72712307+jonx012@users.noreply.github.com>
2022-12-29 00:21:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function contar(){
+    var inicio = document.querySelector("#numInicio")
+    var fim = document.querySelector("#numFim")
+    var passo = document.querySelector("#numPasso")
+    var cont = 0
+    var res = document.querySelector("#res")
+    var inicioN = Number(inicio.value)
+    var fimN = Number(fim.value)
+    var passoN = Number(passo.value)
+
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        alert("[Error] Dado(s) inválido")
+        res.innerHTML = "<p><strong>Não é possível fazer a contagem!</strong></p>"
+    } else {
+        res.innerHTML = "Contando:<br>"
+        if (passoN <= 0) {
+            alert("[Error] Dado(s) inválido")
+            res.innerHTML = "<p><strong>Não é possível fazer a contagem!</strong></p>"
+        } else if (inicioN < fimN) {
+            for (cont = inicioN; cont <= fimN; cont += passoN) {
+                res.innerHTML += ` ${cont} \u{1F449}`
+                res.innerHTML += "\u{1F3C1}"
+            }
+        } else {
+            for (cont = inicioN; cont >= fimN; cont -= passoN) {
+                res.innerHTML += ` ${cont} \u{1F449}`
+                res.innerHTML += "\u{1F3C1}"
+            }
+        }
+    }
+}
